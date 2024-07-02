3505b283aeba942d7d9f431bbb2136cd5c5432bb
mafr <iguacu@post.cz>
2024-07-02 14:54:28 +0200

    přidání formuláře "nový příspěvek" do šablony

--- src/main/java/cz/czechitas/java2webapps/ukol7/controller/PostController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol7/controller/PostController.java
@@ -1,14 +1,14 @@
 package cz.czechitas.java2webapps.ukol7.controller;
 
 import cz.czechitas.java2webapps.ukol7.entity.Post;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import cz.czechitas.java2webapps.ukol7.service.PostService;
 
@@ -21,11 +21,24 @@ public class PostController {
     public PostController(PostService service) {
         this.service = service;
     }
-    @GetMapping("/")
+    @RequestMapping("/")
+    @GetMapping("")
     public ModelAndView index(@RequestParam(required=false,defaultValue = "0")int page){
         PageRequest pageable = PageRequest.of(page, 20);
         Page<Post> listOfPosts = service.seznamPostu(pageable);
-        return new ModelAndView("index").addObject("ListOfPosts",listOfPosts );
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("ListOfPosts",listOfPosts );
+        modelAndView.addObject("post", new Post());
+        return modelAndView;
+    }
+
+    @PostMapping("")
+    public Object form(@ModelAttribute("post") Post post) {
+
+        //if (bindingResult.hasErrors()) {
+        //  return "/";
+        //}
+        return "/index";//OPRAVIT, UPRAVIT
     }
 
     @GetMapping("/post/{slug}")
@@ -33,4 +46,5 @@ public class PostController {
         Post singlePost = service.singlePost(slug);
         return new ModelAndView("detail").addObject("singlePost",singlePost);
     }
+
 }
---
+++ src/main/java/cz/czechitas/java2webapps/ukol7/controller/PostForm.java
@@ -0,0 +1,15 @@
+package cz.czechitas.java2webapps.ukol7.controller;
+
+import java.time.LocalDate;
+
+public class PostForm {
+    private String slug;
+    private String author;
+    private String title;
+    private String perex;
+    private String body;
+    private LocalDate published;
+
+    public PostForm() {
+    }
+}
